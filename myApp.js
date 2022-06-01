551f1e110533b7b129715c20b96819855dea6fbb
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2022-06-01 17:55:09 -0400

    ip req logg

--- myApp.js
+++ myApp.js
@@ -9,6 +9,10 @@ app.use("/public", express.static(__dirname + "/public"));
 
 console.log("Hello World");
 
+app.use(function(req, res, next) {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
