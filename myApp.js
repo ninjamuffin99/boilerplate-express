68402a370871f6441f0f9d8d7a6747a7ce9361fc
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2022-06-01 23:15:53 -0400

    fixed wording

--- myApp.js
+++ myApp.js
@@ -28,7 +28,7 @@ app.get('/:word/echo', function(req, res) {
 
 app.route('/name')
     .get(function(req, res) {
-        res.json({ name: req.query.firstname + " " + req.query.lastname });
+        res.json({ name: req.query.first + " " + req.query.last });
     });
 
 
