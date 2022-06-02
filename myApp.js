5d9ea464e664143addee636cfa3e0f183cdfebdd
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2022-06-01 23:19:46 -0400

    bodyParser boilerplate

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/public", express.static(__dirname + "/public"));
 
